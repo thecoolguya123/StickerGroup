f720266cb60115cbe4f4a40a9eb37121bb20c058
thecoolguya123 <144173586+thecoolguya123@users.noreply.github.com>
2024-02-09 18:21:43 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+    ],
+  },
+};
